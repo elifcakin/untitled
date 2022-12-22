36124853372df9ae74a24ab20a6f61a7fb8e0e57
elif.cakin-at-922294861479 <elif.cakin@defne.com.tr>
2022-12-22 15:37:40 +0300

    ödev1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int numberOne = 10;
-        System.out.println(numberOne);
+        int mat, fizik, kimya, turkce ,tarih, muzik;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("mat:");
+        mat = input.nextInt();
+
+        System.out.println("fizik:");
+        fizik = input.nextInt();
+
+        System.out.println("kimya:");
+        kimya = input.nextInt();
+
+        System.out.println("turkce:");
+        turkce = input.nextInt();
+
+        System.out.println("tarih:");
+        tarih = input.nextInt();
+
+        System.out.println("muzik:");
+        muzik = input.nextInt();
+
+        int toplam = (mat + fizik+kimya+turkce+tarih+muzik);
+        double sonuc = toplam/6.0;
+        System.out.println("ortalama: " + sonuc);
+
+        if (sonuc >60) {
+            System.out.println("sınıfı geçti");
+        }else{
+            System.out.println("sınıfta kaldı");
+        }
+
     }
 
 }
